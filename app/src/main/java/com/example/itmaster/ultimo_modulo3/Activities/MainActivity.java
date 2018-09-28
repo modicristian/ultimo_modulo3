09fa06e963f0cfbe1e6531c7508c2ed0a2074a7d
modicristian <cristianmodi@gmail.com>
2018-09-27 21:38:51 -0300

    subi

--- app/src/main/java/com/example/itmaster/ultimo_modulo3/Activities/MainActivity.java
+++ app/src/main/java/com/example/itmaster/ultimo_modulo3/Activities/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.itmaster.ultimo_modulo3.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 import com.example.itmaster.ultimo_modulo3.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private TableLayout tableLayout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
